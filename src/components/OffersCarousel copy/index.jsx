1fa5e538ac33c6d28101aa4aa34d7d9117fad832
analauracano <laura.fioratti@outlook.com>
2025-11-14 15:18:30 +0000

    utils formatPrice

--- src/components/OffersCarousel copy/index.jsx
+++ src/components/OffersCarousel copy/index.jsx
@@ -5,6 +5,7 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { Container, Title } from './styles';
 import { CardProduct } from '../CardProduct';
+import { formatPrice } from '../../utils/formatPrice';
 
 export function OffersCarousel() {
   const [offers, setOffers] = useState([]);
@@ -13,7 +14,8 @@ export function OffersCarousel() {
     async function loadProducts() {
       const {data} = await api.get('/products');
 
-      const onlyOffers = data.filter((product) => product.offer);
+      const onlyOffers = data.filter((product) => product.offer).map((product) => (
+        {currencyValue: formatPrice(product.price), ...product}));
 
       setOffers(onlyOffers);
     }
