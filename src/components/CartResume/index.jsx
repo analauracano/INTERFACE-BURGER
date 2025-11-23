5e8364121522c8bd4549635c7929eb0d383ffbb2
analauracano <laura.fioratti@outlook.com>
2025-11-23 17:50:41 +0000

    resume

---
+++ src/components/CartResume/index.jsx
@@ -0,0 +1,62 @@
+import { Button } from "../Button";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
+import { useCart } from "../../hooks/CartContext";
+import { api } from "../../services/api";
+import { formatPrice } from "../../utils/formatPrice";
+import { Container } from "./styles";
+import { useNavigate } from "react-router-dom";
+
+export function CartResume(){
+  const [finalPrice, setFinalPrice] = useState(0);
+  const [deliveryTax] = useState(500);
+
+  const { cartProducts, clearCart } = useCart();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const sumAllItems = cartProducts.reduce((acc, current) => {
+      return acc + (current.price * current.quantity);
+    }, 0);
+
+    setFinalPrice(sumAllItems);
+  }, [cartProducts]);
+
+  const submitOrder = async () => {
+  const products = cartProducts.map((product) => ({
+    id: product.id,
+    quantity: product.quantity,
+  }));
+  try{
+  await api.post("/orders", {products
+        },);
+  
+        toast.success("Pedido realizado com sucesso!");
+        navigate("/");
+  
+      } catch (error) {
+        toast.error("Não foi possível realizar o pedido.");
+      }
+      
+      clearCart();
+  };
+
+  return(
+    <div>
+    <Container>
+      <div className="container-top">
+        <h2 className="title">Resumo do Pedido</h2>
+        <p className="items">Itens</p>
+        <p className="items-price">{formatPrice(finalPrice)}</p>
+        <p className="delivery-tax">Taxa de Entrega</p>
+        <p className="delivery-tax-price">{formatPrice(deliveryTax)}</p>
+      </div>
+      <div className="container-bottom">
+        <p>Total</p>
+        <p>{formatPrice(finalPrice + deliveryTax)}</p>
+      </div>
+    </Container>
+    <Button onClick={submitOrder}>Finalizar Pedido</Button>
+    </div>
+  )
+}
--- src/containers/Cart/index.jsx
+++ src/containers/Cart/index.jsx
@@ -1,6 +1,6 @@
-import Logo from '../../assets/Logo 1.svg'
-import { BackButton, CartItems } from '../../components'
-import { Banner, Container, Content, Title } from './styles'
+import Logo from '../../assets/Logo 1.svg';
+import { BackButton, CartItems, CartResume } from '../../components';
+import { Banner, Container, Content, Title } from './styles';
 
 export function Cart(){
   return(
@@ -12,7 +12,7 @@ export function Cart(){
       <Title>Checkout - Pedido</Title>
       <Content>
         <CartItems />
-        {/* <CartResume /> */}
+        <CartResume />
       </Content>
     </Container>
   )
